ef0c2cc72d5eb7ef5fd67d9a30acadaaf7962965
Oluwajoba <jobafash3@gmail.com>
2021-01-13 15:04:45 +0100

    Before Heroku

---
+++ main.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const express = require("express"),
+  layouts = require("express-ejs-layouts"),
+  app = express(),
+  router = require("./routes/index"),
+  morgan = require("morgan"),
+  mongoose = require("mongoose"),
+  methodOverride = require("method-override"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  expressSession = require("express-session"),
+  expressValidator = require("express-validator"),
+  connectFlash = require("connect-flash"),
+  User = require("./models/user");
+
+require('dotenv').config()
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Cuisine API",
+      description: "Cuisine API Information",
+      contact: {
+        name: "Joba Fash"
+      },
+      servers: ["http://localhost:3000"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ["main.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+app.set("port", process.env.PORT);
+app.set("view engine", "ejs");
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+app.use(morgan("combined"));
+app.use(layouts);
+app.use(express.static("public"));
+app.use(expressValidator());
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.use(cookieParser("secretCuisine123"));
+app.use(
+  expressSession({
+    secret: "secretCuisine123",
+    cookie: {
+      maxAge: 4000000
+    },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(connectFlash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  res.locals.flashMessages = req.flash();
+  next();
+});
+
+app.use("/", router);
+
+const server = app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`);
+  }),
+  io = require("socket.io")(server),
+  chatController = require("./controllers/chatController")(io);
